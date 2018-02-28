69a8bb83cc77c4b802013d5317345397c9373ae6
zEduardofaria <dudufaria95@gmail.com>
2018-02-27 21:45:59 -0300

    s3l11

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,9 +2,18 @@ console.log('Startgin app');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash')
+
 const notes = require('./notes.js');
 
-console.log(`Result: ${notes.add(9, -2)}`)
+// console.log(_.isString(true));
+// console.log(_.isString('Eduardo'));
+
+const filteredArray = _.uniq(['Eduardo', 1, 2, 3, 3, 2, 5]);
+console.log(filteredArray);
+
+
+// console.log(`Result: ${notes.add(9, -2)}`)
 
 // var user = os.userInfo();
 
