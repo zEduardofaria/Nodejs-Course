f8aa63d7a2c02bbf95a04383790aa112e63ba72b
zEduardofaria <dudufaria95@gmail.com>
2018-02-27 22:02:22 -0300

    s3l13

--- notes-node/app.js
+++ notes-node/app.js
@@ -4,3 +4,19 @@ const fs = require('fs');
 const _ = require('lodash')
 
 const notes = require('./notes.js');
+
+var command = ProcessingInstruction.argv[2];
+console.log('command: ', command);
+
+if (command === 'add') {
+    console.log('Adding new note');
+} else if (command === 'list') {
+    console.log('Listing all notes');    
+} else if (command === 'read') {
+    console.log('Reading note');
+} else if (command === 'remove') {
+    console.log('Removing note');
+} else {
+    console.log('Command not recognize');
+}
+
