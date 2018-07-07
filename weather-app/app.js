7c8aefa997f887034c3839472ac84f128eacd7e8
zEduardofaria <dudufaria95@gmail.com>
2018-07-06 21:14:59 -0300

    Receiving input information, and add a callback to handle errors

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,34 @@
-const request = require('request')
+const request = require("request");
+const yargs = require("yargs");
 
-request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
-  json: true
-}, (error, response, body) => {
-  console.log(body);  
-})
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: "address",
+      describe: "Address to fetch weather for",
+      string: true
+    }
+  })
+  .help()
+  .alias("help", "h").argv;
+
+const encodedAddress = encodeURIComponent(argv.address);
+
+request(
+  {
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
+    json: true
+  },
+  (error, response, body) => {
+    if (error) {
+      console.log("Unable to connect to Google servers.");
+    } else if (body.status === "ZERO_RESULTS") {
+      console.log("Unable to find that address.");
+    } else if (body.status === "OK") {
+      console.log(`Address: ${body.results[0].formatted_address}`);
+      console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+      console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    }
+  }
+);
