29c7a68b6c737df0a916cec49cfabd949475812e
zEduardofaria <dudufaria95@gmail.com>
2018-07-10 16:46:02 -0300

    Rendering templates with data

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,22 +1,34 @@
 const express = require('express')
+const hbs = require('hbs')
 
 var app = express()
 
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs')
 app.use(express.static(__dirname + '/public'))
 
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+})
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase()
+})
+
 app.get('/', (req, res) => {
-  // res.send('<h1>Hello Express!</h1>');
-  res.send({
-    name: 'Eduardo',
-    likes: [
-      'Learn',
-      'Learn'
-    ]
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website',
+    currentYear: new Date().getFullYear()
   })
 })
 
 app.get('/about', (req, res) => {
-  res.send('About Page')
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  })
 })
 
 app.get('/bad', (req, res) => {
