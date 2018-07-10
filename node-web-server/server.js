8fa8f9f6996c438636ee5a8325850d4ae42f8f85
zEduardofaria <dudufaria95@gmail.com>
2018-07-10 18:36:05 -0300

    Deploying in Heroku

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const hbs = require('hbs')
 const fs = require('fs')
 
+const port = process.env.PORT || 3001
+
 var app = express()
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -55,4 +57,6 @@ app.get('/bad', (req, res) => {
   })
 })
 
-app.listen(3001)
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+})
