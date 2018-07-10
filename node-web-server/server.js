251794e5525a153fdb15f5c9d4a2479c55a296c9
zEduardofaria <dudufaria95@gmail.com>
2018-07-10 18:08:30 -0300

    Playing with express middlewares

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,12 +1,30 @@
 const express = require('express')
 const hbs = require('hbs')
+const fs = require('fs')
 
 var app = express()
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs')
-app.use(express.static(__dirname + '/public'))
 
+app.use((req, res, next) => {
+  const now = new Date().toString()
+  const log = `${now}: ${req.method} ${req.url}`
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log.');
+    }
+  })  
+  next()
+})
+
+// app.use((req, res, next) => {
+//   res.render('maintance.hbs')
+// })
+
+app.use(express.static(__dirname + '/public'))
 
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear()
@@ -37,4 +55,4 @@ app.get('/bad', (req, res) => {
   })
 })
 
-app.listen(3000)
+app.listen(3001)
