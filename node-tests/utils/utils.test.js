c8a01af30ddf0cdbef1b9bfb1ef01438f71473ee
zEduardofaria <dudufaria95@gmail.com>
2018-07-15 12:31:23 -0300

    Playing with async tests, and express server configured to node-tests

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,23 +1,35 @@
-const expected = require('expect')
+const expect = require('expect')
 
 const utils = require('./utils')
 
 it('Should add two numbers', () => {
   const res = utils.add(33, 11)
 
-  expected(res).toBe(44).toBeA('number')
+  expect(res).toBe(44).toBeA('number')
+})
+
+it('Should async add two numbers', (done) => {
+  utils.asyncAdd(4, 3, (sum) => {
+    expect(sum).toBe(7).toBeA('number')
+    done()
+  })
 })
 
 it('Should square a number', () => {
   const res = utils.square(3)
   
-  expected(res).toBe(9).toBeA('number')
+  expect(res).toBe(9).toBeA('number')
+})
+
+it('Should async square a number', (done) => {
+  utils.squareAsync(3, (res) => {
+    expect(res).toBe(9).toBeA('number')
+    done()
+  })
 })
 
 it('Should expect some values', () => {
-  // expected({ name: 'Eduardo' }).toNotEqual({ name: 'Eduardo' })
-  // expected([1,2,3]).toExclude(4)
-  expected({
+  expect({
     name: 'Eduardo',
     age: 22,
     location: 'Philadephia'
@@ -30,7 +42,7 @@ it('Should verify first and last name are set', () => {
   const user = {location: 'Betim', age: 22}
   const res = utils.setName(user, 'Eduardo Faria')
 
-  expected(res).toInclude({
+  expect(res).toInclude({
     firstName: 'Eduardo',
     lastName: 'Faria'
   })
